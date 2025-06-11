eede19c211e037a3df247f7c66b29e6d285ba984
manyakalra05 <146841072+manyakalra05@users.noreply.github.com>
2025-06-11 18:18:12 +0530

    Add files via upload

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,150 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+
+const projects = [
+  {
+    id: 1,
+    title: "Razorpay Clone",
+    description: "A beautiful clone of the Razorpay website built to demonstrate usage of TailwindCSS.",
+    image: "/projects/project1.png",
+    tags: ["HTML", "TailwindCSS"],
+    demoUrl: "https://manyakalra05-razorpay.netlify.app/",
+    githubUrl: "https://github.com/manyakalra05/razorpay  ",
+  },
+  {
+    id: 2,
+    title: "My Portfolio",
+    description:
+      "A stellar personal portfolio demonstrating proficiency in ReactJS and TailwindCSS",
+    image: "/projects/project2.png",
+    tags: ["TailwindCSS", "React.js"],
+    demoUrl: "#",
+    githubUrl: "#",
+  },
+
+   {
+    id: 3,
+    title: "MovieLand",
+    description: "A small website to search for your favourite movies! Bult using ReactJS and OMDB API",
+    image: "/projects/project3.png",
+    tags: ["HTML", "TailwindCSS", "OMDB API"],
+    demoUrl: "https://manyakalra05-movieland.netlify.app/",
+    githubUrl: "https://github.com/manyakalra05/movieapp",
+  },
+
+   {
+    id: 4,
+    title: "Amazon Clone",
+    description: "An clone of the Amazon.com website built to demonstrate efficent usage of JavaScript.",
+    image: "/projects/project4.png",
+    tags: ["HTMl", "JavaScript", "CSS"],
+    demoUrl: "https://manyakalra05-amazon.netlify.app/",
+    githubUrl: "https://github.com/manyakalra05/amazonproj",
+  },
+   {
+    id: 5,
+    title: "Rock Paper Scissors",
+    description: "Play Rock-Paper-Scissors with a computer, using fun emoji's!",
+    image: "/projects/project5.png",
+    tags: ["HTMl", "JavaScript", "CSS"],
+    demoUrl: "https://manyakalra05-rockpaperscissors.netlify.app/",
+    githubUrl: "https://github.com/manyakalra05/rockpaperscissors",
+  },
+
+   {
+    id: 6,
+    title: "Flower.",
+    description: "Browse through various beautiful flowers, and send one to a love one! Heavy Usage of Vanilla CSS",
+    image: "/projects/project6.png",
+    tags: ["CSS", "HTML"],
+    demoUrl: "https://manyakalra05-flowershop.netlify.app/",
+    githubUrl: "https://github.com/manyakalra05/flowershop",
+  },
+
+   {
+    id: 7,
+    title: "Flappy JS",
+    description: "Can you save the JS Bird from falling onto the poles? Press Spacebar to Play!",
+    image: "/projects/project7.png",
+    tags: ["HTMl", "JavaScript", "CSS"],
+    demoUrl: "https://manyakalra05-flappybird.netlify.app/",
+    githubUrl: "https://github.com/manyakalra05/flappybird",
+  },
+
+];
+
+export const ProjectsSection = () => {
+  return (
+    <section id="projects" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          {" "}
+          Featured <span className="text-primary"> Projects </span>
+        </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Here are some of my recent projects. Feel free to explore!
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project, key) => (
+            <div
+              key={key}
+              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+            >
+              <div className="h-48 overflow-hidden">
+                <img
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="p-6">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag) => (
+                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+
+                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}
+                </p>
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-3">
+                    <a
+                      href={project.demoUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <ExternalLink size={20} />
+                    </a>
+                    <a
+                      href={project.githubUrl}
+                      target="_blank"
+                      className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                    >
+                      <Github size={20} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-12">
+          <a
+            className="cosmic-button w-fit flex items-center mx-auto gap-2"
+            target="_blank"
+            href="https://github.com/manyakalra05"
+          >
+            Check My Github <ArrowRight size={16} />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
